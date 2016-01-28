86dedd7822bfc0605f0ea365d30720199b0f61e6
tangbinbin <tangbinbin@didichuxing.com>
2016-01-28 18:55:05 +0800

    fix

--- src/dbstatus/main.go
+++ src/dbstatus/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	host     = flag.String("h", "127.0.0.1:3306", "host")
+	host     = flag.String("h", "127.0.0.1:3306", "hosts,多个地址之间,分割")
 	user     = flag.String("u", "test", "user")
 	password = flag.String("p", "test", "password")
 )
@@ -21,33 +21,48 @@ func init() {
 	flag.Parse()
 }
 
+type Info struct {
+	questions    uint64
+	comSelect    uint64
+	comInsert    uint64
+	comUpdate    uint64
+	comDelete    uint64
+	rowsInsert   uint64
+	rowsSelect   uint64
+	rowsUpdate   uint64
+	rowsDelete   uint64
+	threadCreate uint64
+}
+
+type Server struct {
+	conn *sql.DB
+	info *Info
+}
+
 func main() {
-	db, e := sql.Open("mysql",
-		fmt.Sprintf("%s:%s@tcp(%s)/information_schema",
-			*user, *password, *host))
-	if e != nil {
-		log.Fatal(e)
-		return
-	}
-	if e := db.Ping(); e != nil {
-		log.Fatal(e)
-		return
-	}
-	defer db.Close()
-	type status struct {
-		questions    uint64
-		comSelect    uint64
-		comInsert    uint64
-		comUpdate    uint64
-		comDelete    uint64
-		rowsInsert   uint64
-		rowsSelect   uint64
-		rowsUpdate   uint64
-		rowsDelete   uint64
-		threadCreate uint64
+	addrs := strings.Split(*host, ",")
+	servers := make(map[string]*Server)
+	for _, addr := range addrs {
+		db, e := sql.Open("mysql",
+			fmt.Sprintf("%s:%s@tcp(%s)/information_schema",
+				*user, *password, addr))
+		if e != nil {
+			log.Fatal(e)
+			return
+		}
+		if e := db.Ping(); e != nil {
+			log.Fatal(e)
+			return
+		}
+		server := &Server{
+			conn: db,
+			info: new(Info),
+		}
+
+		servers[addr] = server
 	}
 
-	handler := func(sta *status, db *sql.DB) {
+	handler := func(addr string, sta *Info, db *sql.DB) {
 		ssql := "show global status where variable_name in " +
 			"('Questions', 'Com_select', 'Com_update', " +
 			"'Com_insert', 'Com_delete', 'Threads_connected', " +
@@ -110,23 +125,32 @@ func main() {
 			return
 		}
 		timeNow := strings.Split(strings.Fields(time.Now().String())[1], ".")[0]
-		fmt.Println(fmt.Sprintf("%s|%5d%6d%6d%7d%7d|%5d%6d%6d%7d|%4d%5d%5d",
-			timeNow, ins, upd, del, sel, qps, rin, rup, rdel, rre, run, con, cre))
+		fmt.Println(fmt.Sprintf("%21s %s|%5d%6d%6d%7d%7d|%5d%6d%6d%7d|%4d%5d%5d",
+			addr, timeNow, ins, upd, del, sel, qps, rin, rup, rdel, rre, run, con, cre))
 	}
-	state := new(status)
 	var t int = 0
 	for range time.NewTicker(time.Second).C {
-		switch t % 15 {
+		switch t % 5 {
 		case 0:
-			fmt.Println(strings.Repeat("_", 80))
-			fmt.Println(fmt.Sprintf("--------|%19s%12s| %s |%12s--",
+			fmt.Println("    ", strings.Repeat("_", 98))
+			fmt.Println(fmt.Sprintf("                              |%19s%12s| %s |%12s--",
 				"--QPS--", " ", "--Innodb Rows Status--", "--Thead"))
-			fmt.Println(fmt.Sprintf("  time  |%5s%6s%6s%7s%7s|%5s%6s%6s%7s|%4s%5s%5s",
+			fmt.Println(fmt.Sprintf("          addr          time  |%5s%6s%6s%7s%7s|%5s%6s%6s%7s|%4s%5s%5s",
 				"ins", "upd", "del", "sel", "qps", "ins", "upd", "del", "read", "run", "con", "cre"))
-			go handler(state, db)
+			for addr, server := range servers {
+				handler(addr, server.info, server.conn)
+			}
+			if len(servers) > 1 {
+				fmt.Println()
+			}
 			t++
 		default:
-			go handler(state, db)
+			for addr, server := range servers {
+				handler(addr, server.info, server.conn)
+			}
+			if len(servers) > 1 {
+				fmt.Println()
+			}
 			t++
 		}
 	}
